79be60b4666c19586257327d09045929adbdf583
Ericsson <manikeeric@gmail.com>
2026-06-02 16:03:52 +0000

    AUTH SERVER

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
+import {  NextRequest } from "next/server";
+
+const handler = NextAuth(authOptions);
+
+async function POST(req: Request, ctx: any) {
+
+    const bodyText = await req.text();
+    
+    const newReq = new NextRequest(req.url, {
+        method: req.method,
+        headers: req.headers,
+        body: bodyText,
+    });
+
+    return handler(newReq, ctx);
+}
+
+export { handler as GET, POST };
